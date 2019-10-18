04648f6e7c294745dd5fafe063450a43baf4a620
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-10-17 19:34:54 -0600

    some cheinyes

--- proyecto2.erl
+++ proyecto2.erl
@@ -1,5 +1,5 @@
 -module(proyecto2).
--export([push/2,pop/1,carte/1,k_esimo/2,random/2,generarlista/2,longitud/1]).
+-export([push/2,pop/1,carte/1,k_esimo/2,random/2,generarlista/2,longitud/1,estavacia/1,reverse/1,compare/2]).
 
 
 push(X,L)->[X|L].
@@ -13,6 +13,7 @@ k_esimo([_H|T],K)->k_esimo(T,K-1).
 
 generarlista([],_N)->[];
 generarlista(L,N)->random(carte(L),N).
+%generarlista(L,N)->carte(random(L,N)).
 
 carte(A)->[{X,Y}||X<-A,Y<-A,X>=Y].
 
@@ -24,5 +25,11 @@ random(L,K)->Rand = k_esimo(L,round(rand:uniform()*(length(L)-1))),
 longitud([])->0;
 longitud([_H|T])->1+longitud(T).
 
+estavacia([])->true;
+estavacia(_A)->false.
+
+reverse({H,T})->{T,H}.
+
+compare({A,B},{B,A})->[{A,B},{B,A}].
 
 
